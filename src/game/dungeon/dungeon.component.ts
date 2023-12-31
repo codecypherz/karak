7f90080e725087fd1e449ea7d446cc6c4cc96ea9
codecypherz <james.deyerle@gmail.com>
2023-12-31 15:58:55 -0500

    Support touch dragging on the dungeon.

--- src/game/dungeon/dungeon.component.ts
+++ src/game/dungeon/dungeon.component.ts
@@ -10,31 +10,66 @@ export class DungeonComponent {
 
   @Input() dungeon!: Dungeon;
 
-  mouseDown = false;
+  draggingStarted = false;
   startX = 0;
   startY = 0;
   scrollLeft = 0;
   scrollTop = 0;
 
-  startDragging(e: MouseEvent, flag: boolean, container: HTMLElement): void {
-    this.mouseDown = true;
-    this.startX = e.pageX - container.offsetLeft;
-    this.startY = e.pageY - container.offsetTop;
+  startMouseDragging(e: MouseEvent, flag: boolean, container: HTMLElement): void {
+    this.startDragging(e.pageX, e.pageY, container);
+  }
+
+  startTouchDragging(e: TouchEvent, flag: boolean, container: HTMLElement): void {
+    // Only handle single touch events.
+    if (e.touches.length != 1) {
+      return;
+    }
+    let touch = e.touches[0];
+    this.startDragging(touch.pageX, touch.pageY, container);
+  }
+
+  private startDragging(pageX: number, pageY: number, container: HTMLElement): void {
+    this.draggingStarted = true;
+    this.startX = pageX - container.offsetLeft;
+    this.startY = pageY - container.offsetTop;
     this.scrollLeft = container.scrollLeft;
     this.scrollTop = container.scrollTop;
   }
 
-  stopDragging(e: MouseEvent, flag: boolean): void {
-    this.mouseDown = false;
+  stopMouseDragging(e: MouseEvent, flag: boolean): void {
+    this.stopDragging();
   }
 
-  moveEvent(e: MouseEvent, container: HTMLElement): void {
+  stopTouchDragging(e: TouchEvent, flag: boolean): void {
+    this.stopDragging();
+  }
+
+  private stopDragging(): void {
+    this.draggingStarted = false;
+  }
+
+  moveMouseEvent(e: MouseEvent, container: HTMLElement): void {
     e.preventDefault();
-    if (!this.mouseDown) {
+    this.moveEvent(e.pageX, e.pageY, container);
+  }
+
+  moveTouchEvent(e: TouchEvent, container: HTMLElement): void {
+    // Only handle single touch events.
+    if (e.touches.length != 1) {
       return;
     }
-    const x = e.pageX - container.offsetLeft;
-    const y = e.pageY - container.offsetTop;
+    let touch = e.touches[0];
+    e.preventDefault();
+    this.moveEvent(touch.pageX, touch.pageY, container);
+  }
+
+  private moveEvent(pageX: number, pageY: number, container: HTMLElement): void {
+    if (!this.draggingStarted) {
+      return;
+    }
+    const x = pageX - container.offsetLeft;
+    const y = pageY - container.offsetTop;
     const scrollX = x - this.startX;
     const scrollY = y - this.startY;
     container.scrollLeft = this.scrollLeft - scrollX;
